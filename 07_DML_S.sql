fb48c0b1c9ae0265e535b4b35acc875a99988864
mein0121 <45719589+mein0121@users.noreply.github.com>
2021-02-05 17:53:39 +0900

    SQL_DML_DDL

--- 07_DML_S.sql
+++ 07_DML_S.sql
@@ -1,4 +1,7 @@
 /* *********************************************************************
+DML - 데이터(값)을 다루는 SQL문
+    - INSERT, SELECT(DQL), UPDATE, DELETE : CRUD
+
 INSERT 문 - 행 추가
 구문
  - 한행추가 :
@@ -12,8 +15,27 @@ INSERT 문 - 행 추가
 	
   
 ************************************************************************ */
+DESC DEPT;
+INSERT INTO DEPT (DEPT_ID, DEPT_NAME, LOC) VALUES (1000, '기획부', '서울');
+INSERT INTO DEPT VALUES (1100, '구매부', '부산');
+ROLLBACK; -- INSERT/UPDATE/DELETE 하기 전 상태로 돌리는 명령어.
+COMMIT; -- COMMIT전이면 ROLLBACK으로 데이터 복구(복원)가능.
+
+INSERT INTO DEPT VALUES (1100, '구매부', '부산');
+INSERT INTO DEPT VALUES (1200, '구매부', '부산');
+INSERT INTO DEPT VALUES (1300, '구매부', '부산');
 
 
+INSERT INTO EMP VALUES(1000, '홍길동', 'FI_ACCOUNT', NULL, '2020/01/02', 7000, NULL, 10);
+INSERT INTO EMP(EMP_ID, EMP_NAME, HIRE_DATE, SALARY) VALUES (1100, '이순신', '2000/01/05', 6000);
+INSERT INTO EMP VALUES(1200, '강길동', 'FI_ACCOUNT', NULL,TO_DATE('2020/01', 'YYYY/MM'), 7000, NULL, 10);
+
+INSERT INTO EMP VALUES(1300, '나길동', 'FI_ACCOUNT', 100, '2020/02/01', 9999, 0.1, 10); -- 이미 있는 PK값을 INSERT
+
+SELECT * FROM DEPT ORDER BY DEPT_ID DESC;
+
+
+SELECT * FROM EMP ORDER BY EMP_ID DESC;
 --TODO: 부서별 직원의 급여에 대한 통계 테이블 생성. 
 --      조회결과를 insert. 집계: 합계, 평균, 최대, 최소, 분산, 표준편차
 create table salary_stat(
@@ -25,8 +47,21 @@ create table salary_stat(
     salary_var number(20,2),
     salary_stddev number(7,2)
 );
+INSERT INTO SALARY_STAT
+SELECT DEPT_ID,
+       SUM(SALARY),
+       ROUND(AVG(SALARY),2),
+       MAX(SALARY),
+       MIN(SALARY),
+       ROUND(VARIANCE(SALARY),2),
+       ROUND(STDDEV(SALARY),2)
+FROM EMP
+GROUP BY DEPT_ID
+ORDER BY DEPT_ID;
 
-
+SELECT * FROM SALARY_STAT;
+ROLLBACK;
+COMMIT;
 /* *********************************************************************
 UPDATE : 테이블의 컬럼의 값을 수정
 UPDATE 테이블명
@@ -38,33 +73,101 @@ SET    변경할 컬럼 = 변경할 값  [, 변경할 컬럼 = 변경할 값]
  - WHERE: 변경할 행을 선택. 
 ************************************************************************ */
 
+create table emp_copy(
+        emp_id number(6) ,
+        emp_name varchar2(20),
+        salary number(7,2)
+)
+-- 원래 있던 테이블에서 특정 컬럼만 복사할때
 
 
 -- 직원 ID가 200인 직원의 급여를 5000으로 변경
 
+UPDATE EMP
+SET SALARY = 5000
+WHERE EMP_ID = 200;
+COMMIT;
+
+SELECT * FROM EMP WHERE EMP_ID = 200;
 
 -- 직원 ID가 200인 직원의 급여를 10% 인상한 값으로 변경.
 
+UPDATE EMP
+SET SALARY = SALARY *1.1
+WHERE EMP_ID = 200;
+COMMIT;
 
 -- 부서 ID가 100인 직원의 커미션 비율을 0.2로 salary는 3000을 더한 값으로, 상사_id는 100 변경.
+
+SELECT * FROM  EMP WHERE EMP_ID = 100;
+100	Steven	AD_PRES		2003/06/17	24000		90
+
+UPDATE EMP
+SET COMM_PCT = 0.2, SALARY = SALARY + 3000, MGR_ID = 100
+WHERE EMP_ID = 100;
+
+SELECT * FROM  EMP WHERE EMP_ID = 100;
+100	Steven	AD_PRES	100	2003/06/17	27000	0.2	90
+COMMIT;
 
 
 
 -- TODO: 부서 ID가 100인 직원들의 급여를 100% 인상
-
-
+ SELECT * FROM  EMP WHERE EMP_ID = 100;
+ 100	Steven	AD_PRES	100	2003/06/17	27000	0.2	90
+ UPDATE EMP SET SALARY = SALARY * 2 WHERE EMP_ID = 100;
+100	Steven	AD_PRES	100	2003/06/17	54000	0.2	90
 -- TODO: IT 부서의 직원들의 급여를 3배 인상
+103	Alexander	IT_PROG	102	2006/01/03	9000		60
+104	Bruce	IT_PROG	103	2007/05/21	6000		60
+105	David	IT_PROG	103	2005/06/25	4800		60
+106	Valli	IT_PROG	103	2006/02/05	4800		60
+107	Diana	IT_PROG	103	2007/02/07	4200		60
 
+SELECT * FROM  EMP WHERE DEPT_ID = 60;
 
--- TODO: EMP2 테이블의 모든 데이터를 MGR_ID는 NULL로 HIRE_DATE 는 현재일시로 COMM_PCT는 0.5로 수정.
+103	Alexander	IT_PROG	102	2006/01/03	27000		60
+104	Bruce	IT_PROG	103	2007/05/21	18000		60
+105	David	IT_PROG	103	2005/06/25	14400		60
+106	Valli	IT_PROG	103	2006/02/05	14400		60
+107	Diana	IT_PROG	103	2007/02/07	12600		60
 
+SELECT * FROM DEPT;
+UPDATE EMP 
+SET SALARY = SALARY *3 
+WHERE DEPT_ID IN (SELECT DEPT_ID
+                FROM DEPT
+                WHERE DEPT_NAME = 'IT');
+                        
+ROLLBACK;
+
+-- TODO: EMP 테이블의 모든 데이터를 MGR_ID는 NULL로 HIRE_DATE 는 현재일시로 COMM_PCT는 0.5로 수정.
+
+UPDATE EMP
+SET MGR_ID = NULL,
+    HIRE_DATE = SYSDATE,
+    COMM_PCT = 0.5;
+
+SELECT * FROM EMP;
+
+ROLLBACK;
 
 -- TODO: COMM_PCT 가 0.3이상인 직원들의 COMM_PCT를 NULL 로 수정.
+SELECT EMP_NAME, COMM_PCT FROM EMP;
 
+UPDATE EMP
+SET COMM_PCT = NULL
+WHERE COMM_PCT > 0.3;
+ROLLBACK;
 
 -- TODO: 전체 평균급여보다 적게 받는 직원들의 급여를 50% 인상.
-
-
+SELECT EMP_NAME, SALARY FROM EMP ORDER BY SALARY;
+SELECT SALARY FROM EMP WHERE SALARY < (SELECT AVG(SALARY) FROM EMP);
+UPDATE EMP
+SET SALARY = SALARY *1.5
+WHERE SALARY < (SELECT AVG(SALARY)
+                FROM EMP);
+ROLLBACK;
 /* *********************************************************************
 DELETE : 테이블의 행을 삭제
 구문 
@@ -72,24 +175,47 @@ DELETE : 테이블의 행을 삭제
    - WHERE: 삭제할 행을 선택
 ************************************************************************ */
 -- 부서테이블에서 부서_ID가 200인 부서 삭제
-
-
+DELETE FROM EMP;
+SELECT * FROM EMP WHERE DEPT_ID = 200;
+ROLLBACK;
 -- 부서테이블에서 부서_ID가 10인 부서 삭제
 
+DELETE FROM EMP WHERE DEPT_ID = 200;
+SELECT * FROM EMP WHERE DEPT_ID = 10;
 
 
 -- TODO: 부서 ID가 없는 직원들을 삭제
 
+SELECT * FROM EMP WHERE DEPT_ID IS NULL;
 
+DELETE FROM EMP WHERE DEPT_ID IS NULL;
 
+ROLLBACK;
 -- TODO: 담당 업무(emp.job_id)가 'SA_MAN'이고 급여(emp.salary) 가 12000 미만인 직원들을 삭제.
+SELECT * FROM EMP WHERE JOB_ID IN 'SA_MAN' AND SALARY < 12000;
+DELETE FROM EMP WHERE JOB_ID IN 'SA_MAN' AND SALARY < 12000;
 
-
-
+ROLLBACK;
 -- TODO: comm_pct 가 null이고 job_id 가 IT_PROG인 직원들을 삭제
 
+SELECT * FROM EMP WHERE COMM_PCT IS NULL AND JOB_ID IN 'IT_PROG';
 
-
+DELETE FROM EMP WHERE COMM_PCT IS NULL AND JOB_ID IN 'IT_PROG';
+ROLLBACK;
 -- TODO: job_id에 CLERK가 들어간 업무를 하는 직원들 삭제
 
+SELECT * FROM EMP WHERE JOB_ID LIKE '%CLERK%';
+
+DELETE FROM EMP WHERE JOB_ID LIKE '%CLERK%';
+
+ROLLBACK;
+
+-- EMP_ID 100번인 JOB_ID를 지울때 -- DELETE가 아닌 UPDATE
+SELECT * FROM EMP WHERE EMP_ID = 100;
+
+UPDATE EMP
+SET JOB_ID = NULL
+WHERE EMP_ID = 100;
+
+ROLLBACK;
 
--- 08_DDL_S.sql
+++ 08_DDL_S.sql
@@ -1,8 +1,10 @@
 
 /* ***********************************************************************************
+DDL = DATABASE에 객체(테이블, 유저, 시퀀스 등)를 관리한다.
+- 생성(CREATE), 수정(ALTER), 삭제(DROP)
 테이블 생성
 - 구문
-create table 테이블 이름(
+create table 테이블_이름(
   컬럼 설정
 )
 
@@ -21,9 +23,21 @@ create table 테이블 이름(
 DROP TABLE 테이블이름 [CASCADE CONSTRAINTS]
 *********************************************************************************** */
 
+CREATE TABLE PARENT_TB(
+    NO NUMBER CONSTRAINT PK_PARENT_TB PRIMARY KEY, -- 컬럼 이름을 직점 지정 
+    -- NO NUMBER PRIMARY KEY -- 컬럼 이름을 디폴트값으로 지정
+    NAME NVARCHAR2(30) NOT  NULL,
+    BIRTHDAY DATE DEFAULT SYSDATE, -- 기본값 설정: INSERT시 값을 넣지 않으면 기본값(SYSDATE)으로.
+    EMAIL VARCHAR2(100) CONSTRAINT UK_PARENT_TB_EMAIL UNIQUE, -- 유니크키(중복된 값이 들어갈수없다, null제외) 설정시 컬럼이름 지정.
+    -- EMAIL VARCHAR2(100) UNIQUE, -- 컬럼이름을 디폴트값으로 지정.
+    GENDER CHAR(1) NOT NULL CONSTRAINT CK_PARENT_TB_GENDER CHECK(GENDER IN ('M','F'))
+    -- CHECK 제약조건: 특정 값만 가질수있도록 설정.
+);
 
-
-
+insert into PARENT_TB values(1, '홍길동', '2000/01/01', 'a@a.com', 'M');
+INSERT INTO parent_tb (no, name, gender) values (2, '이순신', 'M');
+insert into PARENT_TB values(3, '홍길동1', null, 'b@a.com', 'M');
+select * from parent_tb;
 
 /* ************************************************************************************
 ALTER : 테이블 수정
@@ -73,73 +87,95 @@ ALTER : 테이블 수정
 
 --TODO: emp 테이블을 카피해서 emp2를 생성(틀만 카피)
 
+CREATE TABLE EMP2
+AS 
+SELECT * FROM EMP WHERE 1 !=1;
 
+SELECT *FROM EMP2;
+DESC EMP2;
 
 --TODO: gender 컬럼을 추가: type char(1)
 
-
+ALTER TABLE EMP2 ADD (GENDER CHAR(1));
 
 --TODO: email 컬럼 추가. type: varchar2(100),  not null  컬럼
 
-
+ALTER TABLE EMP2 ADD (EMAIL VARCHAR2(100) NOT NULL);
 
 --TODO: jumin_num(주민번호) 컬럼을 추가. type: char(14), null 허용. 유일한 값을 가지는 컬럼.
 
-
+ALTER TABLE EMP2 ADD (JUMIN_NUM CHAR(14) CONSTRAINT UK_EMP2_JUMIN UNIQUE);
+DESC EMP2;
+SELECT * FROM USER_CONSTRAINTS WHERE TABLE_NAME = 'EMP2';
 
 --TODO: emp_id 를 primary key 로 변경
 
+ALTER TABLE EMP2 ADD CONSTRAINTS PK_EMP2 PRIMARY KEY(EMP_ID);
 
-  
+
 --TODO: gender 컬럼의 M, F 저장하도록  제약조건 추가
 
-
+ALTER TABLE EMP2 ADD CONSTRAINTS CK_EMP2_GENDER CHECK(GENDER IN ('M', 'F'));
  
 --TODO: salary 컬럼에 0이상의 값들만 들어가도록 제약조건 추가
+
+ALTER TABLE EMP2 ADD CONSTRAINTS CK_EMP2_SALARY CHECK(SALARY > 0);
 
 
 --TODO: email 컬럼을 null을 가질 수 있되 다른 행과 같은 값을 가지지 못하도록 제약 조건 변경
 
+ALTER TABLE EMP2 MODIFY(EMAIL NULL);
 
+SELECT * FROM USER_CONSTRAINTS WHERE TABLE_NAME = 'EMP2';
 
 --TODO: emp_name 의 데이터 타입을 varchar2(100) 으로 변환
 
-
+ALTER TABLE EMP2 MODIFY(EMP_NAME VARCHAR2(100));
+DESC EMP2;
 
 --TODO: job_id를 not null 컬럼으로 변경
 
-
+ALTER TABLE EMP2 MODIFY(JOB_ID NOT NULL);
 
 --TODO: dept_id를 not null 컬럼으로 변경
 
+ALTER TABLE EMP2 MODIFY(DEPT_ID NOT NULL);
 
 
 --TODO: job_id  를 null 허용 컬럼으로 변경
 
+ALTER TABLE EMP2 MODIFY(JOB_ID NULL);
 
 
 --TODO: dept_id  를 null 허용 컬럼으로 변경
 
+ALTER TABLE EMP2 MODIFY(DEPT_ID NULL);
+DESC EMP2;
+
+--TODO: 위에서 지정한 uk_emp2_email 제약 조건을 제거
 
 
---TODO: 위에서 지정한 emp2_email_uk 제약 조건을 제거
 
+--TODO: 위에서 지정한 ck_emp2_salary 제약 조건을 제거
 
+alter table emp2 drop constraint ck_emp2_salary;
 
---TODO: 위에서 지정한 emp2_salary_ck 제약 조건을 제거
-
-
+SELECT * FROM USER_CONSTRAINTS WHERE TABLE_NAME = 'EMP2';
 
 --TODO: primary key 제약조건 제거
 
+alter table emp2 drop primary key;
 
 
 --TODO: gender 컬럼제거
 
-
+alter table emp2 drop column gender;
 
 --TODO: email 컬럼 제거
 
+alter table emp2 drop column email;
+
+desc emp2;
 
 
 /* **************************************************************************************************************
@@ -192,44 +228,104 @@ DROP SEQUENCE sequence이름
 ************************************************************************************************************** */
 
 -- 1부터 1씩 자동증가하는 시퀀스
+create sequence dept_id_seq; -- 이름관례: 시퀀스를 사용할 컬럼명_seq
+--seq이름.nextval()
+select DEPT_ID_SEQ.nextval from dual;
+select DEPT_ID_SEQ.currval from dual;
 
 
+
+insert into dept values(DEPT_ID_SEQ.nextval, '새부서', '서울');
+
+insert into dept values(DEPT_ID_SEQ.nextval, '새부서'||DEPT_ID_SEQ.currval, '서울');
+
+select * from dept ORDER BY DEPT_ID;
 
 -- 1부터 50까지 10씩 자동증가 하는 시퀀스
+DROP SEQUENCE EX1_SEQ;
 
+CREATE SEQUENCE EX1_SEQ
+    INCREMENT BY 10
+    MAXVALUE 50;
+    
+SELECT EX1_SEQ.NEXTVAL FROM DUAL;
 
 
 
 -- 100 부터 150까지 10씩 자동증가하는 시퀀스
 
+CREATE SEQUENCE EX2_SEQ
+    INCREMENT BY 10
+    START WITH 100
+    MAXVALUE 150;
 
 
+SELECT EX2_SEQ.NEXTVAL FROM DUAL;
 
--- 100 부터 150까지 2씩 자동증가하되 최대값에 다다르면 순환하는 시퀀스
+-- 100 부터 150까지 20씩 자동증가하되 최대값에 다다르면 순환하는 시퀀스
+-- 순환(CYCLE)할때 증가(INCREMENT BY 양수) : MINVALUE(기본값:1)에서 시작
+-- 순환(CYCLE)할때 감소(INCREMENT BY 음수) : MAXVALUE(기본값:-1)에서 시작
 
+DROP SEQUENCE EX3_SEQ;
 
+CREATE SEQUENCE EX3_SEQ
+    INCREMENT BY 10
+    START WITH 100
+    MAXVALUE 150
+    MINVALUE 100
+    CACHE 5
+    CYCLE;
+
+SELECT EX3_SEQ.NEXTVAL FROM DUAL;
 
 -- -1부터 자동 감소하는 시퀀스
 
+CREATE SEQUENCE EX4_SEQ
+    INCREMENT BY -1; -- 자동감소: START WITH 기본값이 -1
 
+SELECT EX4_SEQ.NEXTVAL FROM DUAL;
 
 
 -- -1부터 -50까지 -10씩 자동 감소하는 시퀀스
 
+CREATE SEQUENCE EX5_SEQ
+    INCREMENT BY -10
+    START WITH -1
+    MINVALUE -50;
 
-
+SELECT EX5_SEQ.NEXTVAL FROM DUAL;
 
 -- 100 부터 -100까지 -100씩 자동 감소하는 시퀀스
 
+CREATE SEQUENCE EX6_SEQ
+    INCREMENT BY -100
+    START WITH 100
+    MINVALUE -100
+    MAXVALUE 100;
 
+SELECT EX6_SEQ.NEXTVAL FROM DUAL;
 
 -- 15에서 -15까지 1씩 감소하는 시퀀스 작성
 
+DROP SEQUENCE EX7_SEQ;
+
+CREATE SEQUENCE EX7_SEQ
+    INCREMENT BY -1
+    START WITH 15
+    MINVALUE -15
+    MAXVALUE 15;
+
+SELECT EX7_SEQ.NEXTVAL FROM DUAL;
 
 
 -- -10 부터 1씩 증가하는 시퀀스 작성
 
+CREATE SEQUENCE EX8_SEQ
+    INCREMENT BY 1
+    START WITH -10
+    MINVALUE -10; --증가 MINVALUE 1이 기본값. 증가: MINVALUE
 
+SELECT EX8_SEQ.NEXTVAL FROM DUAL;
 
 -- Sequence를 이용한 값 insert
 
@@ -242,8 +338,35 @@ DROP SEQUENCE sequence이름
 -- TODO: 부서ID(dept.dept_id)의 값을 자동증가 시키는 sequence를 생성. 10 부터 10씩 증가하는 sequence
 -- 위에서 생성한 sequence를 사용해서  dept_copy에 5개의 행을 insert.
 
+CREATE SEQUENCE DEPT_ID2_SEQ
+    START WITH 10
+    INCREMENT BY 10;
+
+CREATE TABLE DEPT_COPY
+AS
+SELECT * FROM DEPT WHERE 1<>1;
+
+SELECT * FROM DEPT_COPY;
+
+INSERT INTO DEPT_COPY VALUES(DEPT_ID2_SEQ.NEXTVAL, '기획부','서울');
+
+INSERT INTO DEPT_COPY VALUES(DEPT_ID2_SEQ.NEXTVAL, '개발부','서울');
+
+INSERT INTO DEPT_COPY VALUES(DEPT_ID2_SEQ.NEXTVAL, '구매부','서울');
+
+INSERT INTO DEPT_COPY VALUES(DEPT_ID2_SEQ.NEXTVAL, '기획부','서울');
+
+INSERT INTO DEPT_COPY VALUES(DEPT_ID2_SEQ.NEXTVAL, '기획부','서울');
 
 -- TODO: 직원ID(emp.emp_id)의 값을 자동증가 시키는 sequence를 생성. 10 부터 1씩 증가하는 sequence
 -- 위에서 생성한 sequence를 사용해 emp_copy에 값을 5행 insert
 
+CREATE SEQUENCE EMP_ID_SEQ
+    START WITH 10;
+    
+INSERT INTO EMP2 VALUES(EMP_ID_SEQ.NEXTVAL, 'HONG', NULL,NULL,'2021/01/05',30000, NULL,NULL,NULL);
+INSERT INTO EMP2 VALUES(EMP_ID_SEQ.NEXTVAL, 'KANG', NULL,NULL,'2021/01/05',30000, NULL,NULL,NULL);
+INSERT INTO EMP2 VALUES(EMP_ID_SEQ.NEXTVAL, 'KIM', NULL,NULL,'2021/01/05',30000, NULL,NULL,NULL);
 
+SELECT * FROM EMP2;
+COMMIT;
